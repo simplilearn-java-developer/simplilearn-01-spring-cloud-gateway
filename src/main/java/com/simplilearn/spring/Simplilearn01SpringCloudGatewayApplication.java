2f05475181bd99ef5e62c108eada3795f0248e5d
Carlos Zegarra <cgzegarra@gmail.com>
2023-11-25 14:06:41 -0500

    first commit

---
+++ src/main/java/com/simplilearn/spring/Simplilearn01SpringCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Simplilearn01SpringCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Simplilearn01SpringCloudGatewayApplication.class, args);
+	}
+
+}
